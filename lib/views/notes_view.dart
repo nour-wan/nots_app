f066e3c146c7af9f76bfd75e15ca3af49ade5c64
nour_wanli <nourwanly77@gmail.com>
2023-09-06 22:20:12 -1200

    create widgets

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+
+import '../widgets/notes_view_body.dart';
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
+
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+import 'custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children:[
+        Text('Notes',
+          style: TextStyle(
+                fontSize: 28,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+
+    );
+  }
+}
