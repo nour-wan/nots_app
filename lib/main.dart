0ce3bdb35647aad55e1ed162f329952b94caf254
nour_wanli <nourwanly77@gmail.com>
2023-09-06 21:42:48 -1200

    create notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:note_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -8,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home:const NotesView(),
+    );
   }
 }
