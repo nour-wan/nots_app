a47fb44ee31351719ad9f8655a91efdc95636e63
nour wanli <nourwanly77@gmail.com>
2023-10-03 23:37:30 -1200

    add hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,12 @@
-class NoteModel{
+class NoteModel {
   final String title;
   final String subTitle;
   final String date;
   final int color;
 
-  NoteModel({required this.title,required this.subTitle,required this.date,required this.color});
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
 }
